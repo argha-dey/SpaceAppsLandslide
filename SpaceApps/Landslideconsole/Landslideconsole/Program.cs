f0da49028e9236866136e2270e91bf447ba8403b
ik1004 <ik1004@bristol.ac.uk>
2014-04-13 19:57:54 +0300

    Final Submission

---
+++ SpaceApps/Landslideconsole/Landslideconsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LandSlideClass;
+using System.Timers;
+using System.Threading;
+namespace Landslideconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Hour, Minute;
+            double FactorSafety;
+            string data = string.Empty;
+            for (int n = 0; n < 6; n++)
+            {
+                Analysis Analysis1 = new Analysis();
+                Analysis1.CreateData();
+
+                Analysis1.Run(n);
+                FactorSafety = Analysis1.FactorSafety;
+                Hour = Analysis1.Hour;
+                Minute = Analysis1.Minute;
+                Console.WriteLine("Time " + Hour.ToString("00") + ":" + Minute.ToString("00"));
+                data = FactorSafety.ToString("0.00");
+                Console.WriteLine("Factory Of Safety " +data);
+                Thread.Sleep(10000);
+            }
+            System.IO.File.WriteAllText("data.txt","<var1>"+ data +"</var1>");
+            Console.Write("press eny key to exit.");
+            System.Console.ReadKey();
+        }
+
+    }
+}
+
---
+++ SpaceApps/Landslideconsole/OutputData.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace WritingToFile
+{
+    class Program
+    {
+        static void Main()
+        {
+            try
+            {
+                FileStream fs = new FileStream("sample.txt", FileMode.Create);
+                StreamWriter writer = new StreamWriter(fs);
+                StringBuilder output = new StringBuilder();
+                int repeat = 0;
+
+                do
+                {
+                    Console.WriteLine("Please enter first name: ");
+                    output.Append(Console.ReadLine() + "#");
+                    Console.WriteLine("Please enter last name: ");
+                    output.Append(Console.ReadLine() + "#");
+                    Console.WriteLine("Please enter age: ");
+                    output.Append(Console.ReadLine());
+                    writer.WriteLine(output.ToString());
+                    output.Clear();
+                    Console.WriteLine("Repeat? 1-Yes, 0-No : ");
+                    repeat = Convert.ToInt32(Console.ReadLine());
+                } while (repeat != 0);
+
+                writer.Close();
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
